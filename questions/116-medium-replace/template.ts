5faa764bb8c00d354bd828d56698798fb7b7b737
kkopitehong <kkopite.hong@smartcorelink.com>
2022-01-27 11:09:09 +0800

    feat:116,119

--- questions/116-medium-replace/template.ts
+++ questions/116-medium-replace/template.ts
@@ -1,1 +1,15 @@
-type Replace<S extends string, From extends string, To extends string> = any
+// type Replace<S extends string, From extends string, To extends string, F extends string = ''> =
+//   From extends ''
+//     ? S
+//     : S extends `${From}${infer Rest}`
+//       ? `${F}${To}${Rest}`
+//       : S extends `${infer First}${infer Rest}`
+//         ? Replace<Rest, From, To, `${F}${First}`>
+//         : `${F}${S}`
+
+type Replace<S extends string, From extends string, To extends string>
+  = From extends ''
+    ? S
+    : S extends `${infer F}${From}${infer R}`
+      ? `${F}${To}${R}`
+      : S
--- questions/119-medium-replaceall/template.ts
+++ questions/119-medium-replaceall/template.ts
@@ -1,1 +1,22 @@
-type ReplaceAll<S extends string, From extends string, To extends string> = any
+// type ReplaceAll<S extends string, From extends string, To extends string, R extends string = ''> =
+//   From extends ''
+//     ? S
+//     : S extends `${From}${infer Rest}`
+//       ? ReplaceAll<Rest, From, To, `${R}${To}`>
+//       : S extends `${infer First}${infer Rest}`
+//         ? ReplaceAll<Rest, From, To, `${R}${First}`>
+//         : `${R}${S}`
+
+type ReplaceAll<S extends string, From extends string, To extends string>
+  = From extends ''
+    ? S
+    : S extends `${infer F}${From}${infer L}`
+      ? `${F}${To}${ReplaceAll<L, From, To>}`
+      : S
+
+type From = 'hello'
+
+type text = 'hello'
+
+type check<T extends string> = T extends `${infer F}${From}`
+  ? F : T
