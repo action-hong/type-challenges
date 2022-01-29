0e485b0f22742e05830265cd2cc605f7fcd61d96
kkopitehong <kkopite.hong@smartcorelink.com>
2022-01-29 13:01:36 +0800

    feat: 151

--- questions/147-hard-c-printf-parser/template.ts
+++ questions/147-hard-c-printf-parser/template.ts
@@ -1,11 +1,18 @@
 type ControlsMap = {
-  c: 'char',
-  s: 'string',
-  d: 'dec',
-  o: 'oct',
-  h: 'hex',
-  f: 'float',
-  p: 'pointer',
+  c: 'char'
+  s: 'string'
+  d: 'dec'
+  o: 'oct'
+  h: 'hex'
+  f: 'float'
+  p: 'pointer'
 }
 
-type ParsePrintFormat = any
+type ParsePrintFormat<S extends string, R extends string[] = []> =
+  S extends `${infer F}${infer C}${infer Rest}`
+    ? F extends '%'
+      ? C extends keyof ControlsMap
+        ? ParsePrintFormat<Rest, [...R, ControlsMap[C]]>
+        : ParsePrintFormat<Rest, R>
+      : ParsePrintFormat<`${C}${Rest}`, R>
+    : R
--- questions/151-extreme-query-string-parser/template.ts
+++ questions/151-extreme-query-string-parser/template.ts
@@ -1,1 +1,58 @@
-type ParseQueryString = any
+// type MyMerge<A, B> = {
+//   [K in keyof A | keyof B]: K extends keyof A
+//     ? K extends keyof B
+//       ? Equal<A[K], B[K]> extends true ? A[K] : [A[K], B[K]]
+//       : A[K]
+//     : K extends keyof B
+//       ? B[K]
+//       : never
+// }
+
+// type ParseQueryString<S extends string> = S extends ''
+//   ? {}
+//   : S extends `${infer K}=${infer V}&${infer Rest}`
+//     ? MyMerge<{[P in K]: V}, ParseQueryString<Rest>>
+//     : S extends `${infer K}&${infer Rest}`
+//       ? MyMerge<{[P in K]: true}, ParseQueryString<Rest>>
+//       : S extends `${infer K}=${infer V}`
+//         ? { [P in K]: V }
+//         : S extends `${infer K}`
+//           ? { [P in K]: true }
+//           : {}
+// type m151 = ParseQueryString<'k1=v1&k1=v1'>
+
+// type Result<V1, V2> = V1 extends V2 ? V1 : V1 extends [...infer O] ? [...O, V2] : [V1, V2]
+
+// type MergeResult<T, K extends string, V, R = T & { [key in K]: V }> = K extends keyof T
+//   ? { [K1 in keyof T]: K extends K1 ? Result<T[K1], V> : T[K1] }
+//   : { [K2 in keyof R]: R[K2] }
+
+// type ParseKV<S extends string, R> = S extends `${infer K}=${infer V}` ? MergeResult<R, K, V extends '' ? true : V> : '' extends S ? R : MergeResult<R, S, true>
+
+// type ParseQueryString<S extends string, R = {}> = S extends `${infer F}&${infer O}` ? ParseQueryString<O, ParseKV<F, R>> : ParseKV<S, R>
+
+// 思路，分成两步
+// 先分&，再分k=v
+
+type Result<A, B> = A extends B ? A : A extends [...infer O] ? [...O, B]: [A, B]
+
+type MergeResult<T, K extends string, V, R = T & { [key in K]: V }> =
+  K extends keyof T
+    ? {
+      [K1 in keyof T]: Result<T[K1], V>
+    }
+    : {
+      [K2 in keyof R]: R[K2]
+    }
+
+type ParseKV<S extends string, R> =
+  S extends `${infer K}=${infer V}`
+    ? MergeResult<R, K, V extends '' ? true : V>
+    : '' extends S
+      ? R
+      : MergeResult<R, S, true>
+
+type ParseQueryString<S extends string, R = {}> =
+  S extends `${infer F}&${infer O}`
+    ? ParseQueryString<O, ParseKV<F, R>>
+    : ParseKV<S, R>
