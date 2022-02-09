cec8079ae3fa23689b2b66fa1f2081b180d393c4
kkopitehong <kkopite.hong@smartcorelink.com>
2022-02-09 14:05:19 +0800

    feat: 增加了题目

--- questions/296-medium-permutation/template.ts
+++ questions/296-medium-permutation/template.ts
@@ -1,1 +1,6 @@
-type Permutation<T> = any
+type Permutation<T, P = T> =
+  [T] extends [never]
+    ? []
+    : T extends any
+      ? [T, ...Permutation<Exclude<P, T>>]
+      : never
--- questions/55-hard-union-to-intersection/template.ts
+++ questions/55-hard-union-to-intersection/template.ts
@@ -1,1 +1,20 @@
 type UnionToIntersection<U> = (U extends unknown ? (args: U) => unknown : never) extends (args: infer P) => unknown ? P : never
+
+// interface Animal {
+//   name: string
+// }
+
+// interface Dog extends Animal {
+//   bark(): void
+// }
+
+// interface Greyhound extends Dog {
+//   kind: string
+// }
+
+// function test(fn: (arg: Dog) => Dog): void {
+// }
+
+// declare function g(arg: Animal): Greyhound
+
+// test(g)
