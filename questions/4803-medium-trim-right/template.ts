eb0f1f89b0cd3a1135b5d2a62653434dea26bf2b
kkopitehong <kkopite.hong@smartcorelink.com>
2022-02-21 11:27:27 +0800

    3

--- questions/4803-medium-trim-right/template.ts
+++ questions/4803-medium-trim-right/template.ts
@@ -1,1 +1,6 @@
-type TrimRight<S extends string> = any
+// type W = '\n' | '\t' | ' '
+
+type TrimRight<S extends string> =
+  S extends `${infer SS}${W}`
+    ? TrimRight<SS>
+    : S
--- questions/5117-medium-without/template.ts
+++ questions/5117-medium-without/template.ts
@@ -1,1 +1,15 @@
-type Without<T, U> = any
+type CheckIn<T, U> =
+  U extends unknown[]
+    ? T extends U[number]
+      ? true
+      : false
+    : T extends U
+      ? true
+      : false
+
+type Without<T extends unknown[], U, R extends unknown[] = []> =
+  T extends [infer H, ...infer Rest]
+    ? CheckIn<H, U> extends true
+      ? Without<Rest, U, R>
+      : Without<Rest, U, [...R, H]>
+    : R
