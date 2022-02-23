257a309f1c346a04821bb05fd571d6c4a06e64f4
kkopitehong <kkopite.hong@smartcorelink.com>
2022-02-23 10:17:33 +0800

    4471;4484

--- questions/4471-medium-zip/template.ts
+++ questions/4471-medium-zip/template.ts
@@ -1,1 +1,22 @@
-type Zip = any
+// type ArrayPeak<T extends unknown[]>
+//   = T extends [infer H, ...any]
+//     ? H
+//     : never
+
+// type ArrayShift<T extends unknown[]>
+//   = T extends [any, ...infer Rest]
+//     ? Rest
+//     : never
+
+// type Zip<T extends unknown[], N extends unknown[], R extends unknown[][] = []> =
+//   T extends [infer H, ...infer Rest]
+//     ? Zip<Rest, ArrayShift<N>, IsNever<ArrayPeak<N>> extends true ? R : [...R, [H, ArrayPeak<N>]]>
+//     : R
+
+// // type a = Zip<[1, 2, 3], ['1', '2']>
+type Zip<T extends unknown[], N extends unknown[], R extends unknown[][] = []> =
+  T extends [infer TH, ...infer TA]
+    ? N extends [infer NH, ...infer NA]
+      ? Zip<TA, NA, [...R, [TH, NH]]>
+      : R
+    : R
--- questions/4484-medium-istuple/template.ts
+++ questions/4484-medium-istuple/template.ts
@@ -1,1 +1,9 @@
-type IsTuple<T> = any
+type IsTuple<T> =
+  T extends readonly any[]
+    ? number extends T['length']
+      ? false
+      : true
+    : false
+
+// tuple的长度是固定的, 例如 [number] 长度是1
+// 则 number extends 1 为假 取true
