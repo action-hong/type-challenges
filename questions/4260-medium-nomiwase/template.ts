7c93ab58a1acde47328efafc98ee6c5cfa4d7c69
kkopitehong <kkopite.hong@smartcorelink.com>
2022-02-24 15:44:35 +0800

    feat: 4260,4425;

--- questions/4260-medium-nomiwase/template.ts
+++ questions/4260-medium-nomiwase/template.ts
@@ -1,1 +1,16 @@
-type AllCombinations<S> = any
+type String2Union<S extends string> =
+  S extends `${infer H}${infer Rest}`
+    ? H | String2Union<Rest>
+    : ''
+
+type test = String2Union<'HELLO'>
+
+type _Combinations<T, U = T>
+  = [T] extends [never]
+    ? '' : T extends U ? `${T & string}${_Combinations<Exclude<U, T>> | ''}` : ''
+
+// type test1 = _Combinations<'A' | 'B'>
+
+type AllCombinations<S extends string> = _Combinations<String2Union<S>>
+
+type test1 = AllCombinations<'ABC'>
--- questions/4425-medium-greater-than/template.ts
+++ questions/4425-medium-greater-than/template.ts
@@ -1,1 +1,18 @@
-type GreaterThan<T extends number, U extends number> = any
+type NArray<T extends number, N extends number[] = []> = N['length'] extends T
+  ? N
+  : NArray<T, [...N, 0]>
+
+// type GreaterThan<T extends number, U extends number> = T extends U
+//   ? false
+//   : NArray<T> extends [...any, ...NArray<U>]
+//     ? true
+//     : false
+
+type GreaterThan<T extends number, U extends number, TA extends number[] = [], UA extends number[] = []>
+  = T extends U
+    ? false
+    : T extends TA['length']
+      ? false
+      : U extends UA['length']
+        ? true
+        : GreaterThan<T, U, [...TA, 0], [...UA, 0]>
